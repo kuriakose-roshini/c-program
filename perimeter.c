fccf5f7a5bec7495602ca6752b764a40d540065d
kuriakose-roshini <kuriakoseroshini08@gmail.com>
2023-09-12 15:48:57 +0530

    perimeter

--- perimeter.c
+++ perimeter.c
@@ -4,11 +4,6 @@ int main()
     int a,b;
     printf("Enter the sides of your rectangle");
     scanf("%d%d",&a,&b);
-<<<<<<< HEAD
+    int perimeter =2*(a+b);
+    printf("perimeter=%d",perimeter);
 }
-=======
-    int perimeter;
-    perimeter= 2*(a+b);
-    printf("perimeter of the given rectangle is: %d",perimeter);
-}
->>>>>>> 22748e6251d77b9b7808c132cde4aabc9daff5cf
